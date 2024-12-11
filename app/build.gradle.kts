5b56928ab75a6a3fd7591d2f02ae7c255f36bf66
yui <yuineor@gmail.com>
2024-12-11 23:15:23 +0700

    add retrofit dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,8 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.retrofit)
+    implementation(libs.androidx.retrofit.gson)
     implementation(libs.androidx.recyclerview)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
