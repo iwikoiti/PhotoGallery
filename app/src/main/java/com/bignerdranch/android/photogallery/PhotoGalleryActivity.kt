f636432a7dcacec84043a86447178cd154f12411
yui <yuineor@gmail.com>
2024-12-11 22:36:35 +0700

    add PhotoGalleryFragment and dependencies

--- app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryActivity.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryActivity.kt
@@ -11,10 +11,10 @@ class PhotoGalleryActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_photo_gallery)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val isFragmentContainerEmpty = savedInstanceState == null
+        if (isFragmentContainerEmpty) {
+            supportFragmentManager.beginTransaction()
+                .add(R.id.fragmentContainer, PhotoGalleryFragment.newInstance()).commit()
         }
     }
 }
