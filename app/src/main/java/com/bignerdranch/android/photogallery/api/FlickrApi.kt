1ac06a5738b6806d9a098c5d8be5411e0414390c
yui <yuineor@gmail.com>
2024-12-12 02:58:20 +0700

    getting JSON data from Flickr

--- app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrApi.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrApi.kt
@@ -4,6 +4,12 @@ import retrofit2.Call
 import retrofit2.http.GET
 
 interface FlickrApi {
-    @GET("/")
-    fun fetchContents(): Call<String>
+    @GET(
+        "services/rest/?method=flickr.interestingness.getList" +
+        "&api_key=fedd900622f21be01a73bf24a0cb1bf0" +
+                "&format=json" +
+                "&nojsoncallback=1" +
+                "&extras=url_s"
+    )
+    fun fetchPhotos(): Call<String>
 }
