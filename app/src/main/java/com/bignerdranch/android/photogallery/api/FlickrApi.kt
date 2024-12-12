06ba6e12abc76e80ebedc35868791788ea1f7cd4
yui <yuineor@gmail.com>
2024-12-12 23:51:39 +0700

    (chapter 26) Flickr search

--- app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrApi.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrApi.kt
@@ -2,14 +2,17 @@ package com.bignerdranch.android.photogallery.api
 
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface FlickrApi {
     @GET(
-        "services/rest/?method=flickr.interestingness.getList" +
-        "&api_key=fedd900622f21be01a73bf24a0cb1bf0" +
-                "&format=json" +
-                "&nojsoncallback=1" +
-                "&extras=url_s"
+        "services/rest?method=flickr.interestingness.getList"
     )
     fun fetchPhotos(): Call<FlickrResponse>
+
+    //@GET
+    //fun fetchUrlBytes(@Url url: String):Call<ResponseBody>
+
+    @GET("services/rest?method=flickr.photos.search")
+        fun searchPhotos(@Query("text") query: String): Call<FlickrResponse>
 }
