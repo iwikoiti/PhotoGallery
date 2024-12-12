d4528d86693d38bf12db2a7b4dc005b643eba905
yui <yuineor@gmail.com>
2024-12-12 21:55:50 +0700

    passing handlers

--- app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
@@ -1,8 +1,10 @@
 package com.bignerdranch.android.photogallery
 
+import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.ColorDrawable
 import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -28,7 +30,14 @@ class PhotoGalleryFragment: Fragment() {
         super.onCreate(savedInstanceState)
         retainInstance = true
         photoGalleryViewModel = ViewModelProvider(this)[PhotoGalleryViewModel::class.java]
-        thumbnailDownloader = ThumbnailDownloader()
+
+        val responseHandler = Handler()
+        thumbnailDownloader = ThumbnailDownloader(responseHandler){
+            photoHolder, bitmap ->
+            val drawable = BitmapDrawable(resources, bitmap)
+            photoHolder.bindDrawable(drawable)
+        }
+
         lifecycle.addObserver(thumbnailDownloader)
     }
 
--- app/src/main/java/com/bignerdranch/android/photogallery/ThumbnailDownloader.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/ThumbnailDownloader.kt
@@ -1,6 +1,7 @@
 package com.bignerdranch.android.photogallery
 
 import android.annotation.SuppressLint
+import android.graphics.Bitmap
 import android.os.Handler
 import android.os.HandlerThread
 import android.os.Message
@@ -13,7 +14,8 @@ import java.util.concurrent.ConcurrentHashMap
 private const val TAG = "ThumbnailDownloader"
 private const val MESSAGE_DOWNLOAD = 0
 
-class ThumbnailDownloader<in T>: HandlerThread(TAG), LifecycleObserver {
+class ThumbnailDownloader<in T>(private val responseHandler: Handler,
+    private val onThumbnailDownloaded: (T, Bitmap) -> Unit): HandlerThread(TAG), LifecycleObserver {
 
     private var hasQuit = false
     private lateinit var requestHandler: Handler
@@ -61,5 +63,13 @@ class ThumbnailDownloader<in T>: HandlerThread(TAG), LifecycleObserver {
     private fun handleRequest(target: T) {
         val url = requestMap[target] ?: return
         val bitmap = flickrFetchr.fetchPhoto(url) ?: return
+
+        responseHandler.post(Runnable{
+            if (requestMap[target] != url || hasQuit){
+                return@Runnable
+            }
+            requestMap.remove(target)
+            onThumbnailDownloaded(target, bitmap)
+        })
     }
 }
