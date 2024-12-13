53d369fccff693652491c7f32a2aaa0785b20ee1
yui <yuineor@gmail.com>
2024-12-13 14:27:27 +0700

    checking for new photos

--- app/src/main/java/com/bignerdranch/android/photogallery/PollWorker.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/PollWorker.kt
@@ -1,9 +1,15 @@
 package com.bignerdranch.android.photogallery
 
+import android.app.PendingIntent
 import android.content.Context
+import android.content.pm.PackageManager
 import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat
 import androidx.work.Worker
 import androidx.work.WorkerParameters
+import android.Manifest
 
 private const val TAG = "PollWorker"
 
@@ -30,6 +36,29 @@ class PollWorker(val context: Context, workerParams: WorkerParameters): Worker(c
         } else {
             Log.i(TAG, "Got a new result: $resultId")
             QueryPreferences.setLastResultId(context, resultId)
+
+            val intent = PhotoGalleryActivity.newIntent(context)
+            val pendingIntent = PendingIntent.getActivity(context, 0, intent, 0)
+            val resources = context.resources
+            val notification = NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_ID)
+                .setTicker(resources.getString(R.string.new_pictures_title))
+                .setSmallIcon(android.R.drawable.ic_menu_report_image)
+                .setContentTitle(resources.getString(R.string.new_pictures_title))
+                .setContentText(resources.getString(R.string.new_pictures_text))
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true)
+                .build()
+
+            if (ContextCompat.checkSelfPermission(
+                context,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) == PackageManager.PERMISSION_GRANTED){
+                val notificationManager = NotificationManagerCompat.from(context)
+                notificationManager.notify(0, notification)
+            } else {
+                Log.e(TAG, "Notification permission not granted")
+            }
+
         }
 
         return Result.success()
