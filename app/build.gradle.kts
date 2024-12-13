656afa0caa56f50c8b4baa1f8340c93417947bd4
yui <yuineor@gmail.com>
2024-12-13 13:19:43 +0700

    (chapter 27) create class Worker

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,7 @@ dependencies {
     implementation(libs.androidx.recyclerview)
     implementation(libs.androidx.picasso)
     implementation(libs.androidx.lifecycle.livedata)
+    implementation(libs.androidx.work.runtime)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
